17846fb88b1439d465668eeefe8798acba787306
tomosiako <bulimotombrian001@gmail.com>
2026-01-20 23:15:12 +0300

    added a tree and a form view on the patient and view patient menu

--- custom_addons/om_hospital/__manifest__.py
+++ custom_addons/om_hospital/__manifest__.py
@@ -3,9 +3,13 @@
     "author":"Scarface Software Solutions",
     "licence":"LGPL-3",
     "version":"17.0.1.0",
+    "depends":[
+        'mail'
+    ],
     "data":[
         "security/ir.model.access.csv",
         "views/patient_views.xml",
+        "views/patient_readonly_views.xml",
         "views/menu.xml"
 
 
--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -5,9 +5,10 @@ from odoo import api, fields, models
 
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
+    _inherit = ['mail.thread']
     _description = 'Patient Master'
 
-    name = fields.Char(string="Name",required=True)
-    date_of_birth = fields.Date(string="DOB")
-    gender = fields.Selection([('male','Male'),('female','Female')],string="Gender")
+    name = fields.Char(string="Name",required=True, tracking=True)
+    date_of_birth = fields.Date(string="DOB",tracking=True)
+    gender = fields.Selection([('male','Male'),('female','Female')],string="Gender",tracking=True)
 
