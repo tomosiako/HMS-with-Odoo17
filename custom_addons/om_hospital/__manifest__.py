0c3edbfcc44e393c8f0c9192b3f257c7e1a6ac0e
tomosiako <bulimotombrian001@gmail.com>
2026-01-20 21:08:58 +0300

    added the first menu/parent menu

--- custom_addons/om_hospital/__manifest__.py
+++ custom_addons/om_hospital/__manifest__.py
@@ -2,5 +2,12 @@
     "name":"Hospital Management System",
     "author":"Scarface Software Solutions",
     "licence":"LGPL-3",
-    "version":"17.0.1.0"
+    "version":"17.0.1.0",
+    "data":[
+        "security/ir.model.access.csv",
+        "views/patient_views.xml",
+        "views/menu.xml"
+
+
+]
 }
--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -1,7 +1,13 @@
+from pkg_resources import require
+
 from odoo import api, fields, models
 
 
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = 'Patient Master'
-    
+
+    name = fields.Char(string="Name",required=True)
+    date_of_birth = fields.Date(string="DOB")
+    gender = fields.Selection([('male','Male'),('female','Female')],string="Gender")
+
