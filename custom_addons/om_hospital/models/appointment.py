ee9cc56f3a6ef22b8cc2b1a961ad62fe5fb2200b
tomosiako <bulimotombrian001@gmail.com>
2026-01-24 21:42:01 +0300

    added a status bar

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -1,3 +1,5 @@
+from email.policy import default
+
 from pkg_resources import require
 
 from odoo import api, fields, models
@@ -13,6 +15,10 @@ class HospitalAppointment(models.Model):
     patient_id = fields.Many2one('hospital.patient',string="Patient")
     date_appointment = fields.Date(string="Date")
     note = fields.Text(string="Note")
+    state = fields.Selection(
+        [('draft','Draft'),('confirmed','Confirmed'),('ongoing','Ongoing'),
+         ('done','Done'),('cancel','Canceled')
+         ],default='draft',tracking=True)
 
     @api.model_create_multi
     def create(self,vals_list):
@@ -20,3 +26,19 @@ class HospitalAppointment(models.Model):
             if not vals.get('reference') or vals['reference'] == 'New':
                 vals['reference'] = self.env['ir.sequence'].next_by_code('hospital.appointment')
             return  super().create(vals_list)
+
+    def action_confirm(self):
+        for rec in self:
+            rec.state = 'confirmed'
+
+    def action_ongoing(self):
+        for rec in self:
+            rec.state = 'ongoing'
+
+    def action_done(self):
+        for rec in self:
+            rec.state = 'done'
+
+    def action_cancel(self):
+        for rec in self:
+            rec.state = 'cancel'
